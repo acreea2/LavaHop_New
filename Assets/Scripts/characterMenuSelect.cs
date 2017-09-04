0bfb0a2d46f214cfabf6fec6fe02323c24434db8
acreea2 <son.of.robins@gmail.com>
2017-09-04 11:33:23 -0400

    Added exit to everything and attempted to change material alpha on raycast over

--- Assets/Scripts/characterMenuSelect.cs
+++ Assets/Scripts/characterMenuSelect.cs
@@ -5,12 +5,18 @@ public class characterMenuSelect : MonoBehaviour {
 
 	private float range = 1000f;
 	public Camera cam;
+	public Material startBackground;
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0) || Input.GetKeyDown (KeyCode.Space)) {
 			trySelection ();
 		}
+	}
+
+	void OnMouseOver()
+	{
+		print (gameObject.name);
 	}
 
 	void trySelection() {
@@ -20,13 +26,25 @@ public class characterMenuSelect : MonoBehaviour {
 
 			if (hit.transform.name == "MenuStart") {
 				GameManager.instance.startGame ();
+				Debug.Log("Start");
 			}
 			if (hit.transform.tag == "StartLevel") {
 				string level = Regex.Match (hit.transform.parent.name, @"\d+").Value;
 				GameManager.instance.startGame (int.Parse(level));
+				Debug.Log("Start Level");
+
 			}
 			if (hit.transform.tag == "Exit") {
 				GameManager.instance.exit ();
+			}
+
+			if (hit.transform.tag == "StartLevel") {
+
+				Debug.Log("Start Level RayTraced");
+//				Color transparencyColor = new Color(1, 1, 1, 0.01f);
+//				Renderer startBackground;
+//				tempRenderer = theCollision.renderer;
+//				self.startBackground.material.color.a = 0.01f;
 			}
 		}
 	}
